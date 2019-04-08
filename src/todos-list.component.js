f6e4eceaaea59263b380689a93325dc9f75a7fa0
GouravMukherjee <gouravmukherjee09@gmail.com>
2019-04-08 19:49:36 +0530

    added real time data for both components

--- src/todos-list.component.js
+++ src/todos-list.component.js
@@ -1,12 +1,51 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
- class TodoListComponent extends Component {
+class TodoListComponent extends Component {
+   state = {
+      todos: [],
+      loading: true
+   };
+
+   baseUrl = "http://192.168.0.145:3006";
+
+   componentDidMount() {
+      console.log('[ComponentDidMount called]');
+      axios.get(`${this.baseUrl}/api/todo/list`)
+         .then(response => {
+            console.log('[response]', response)
+            this.setState({
+               loading: false,
+               todos: response['data']['todos']
+            })
+         })
+         .catch(error => {
+            console.log('[error occured]', error)
+         })
+   }
    render() {
-      return (
-         <div>
-            Todos list component works!
-         </div>
-      );
+
+      if (this.state.loading) {
+         return <div> Loading...</div>
+      } else {
+         const todos = this.state.todos.map(todo => {
+            return (
+               <div className="card" key={todo._id}>
+                  <div className="card-body">
+                     <h5 className="card-title">{todo.responsible}</h5>
+                     <p className="card-text">{todo.description}</p>
+                     {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+                  </div>
+               </div>
+            );
+         })
+         console.log('todos',todos);
+         return (
+            <div className="container">
+               {todos}
+            </div>
+         );
+      }
    }
 }
 
