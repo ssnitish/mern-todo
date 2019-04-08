58daefe07830b4ccc3c73523614cfcf07ab4e81c
GouravMukherjee <gouravmukherjee09@gmail.com>
2019-04-08 16:51:31 +0530

    made the create todo component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import TodoListComponent from './todos-list.component';
 import EditTodo from "./edit-todo.component";
 import CreateTodo from './create-todo.component'
+import Navbar from './navbar.component';
 import './App.css';
 
 class App extends Component {
    render() {
       return (
-         <div className="container-fluid">
+         <div className="">
             <Router>
+               <Navbar />
                <div className="App">
                   <Route path="/" exact component={TodoListComponent} />
                   <Route path="/edit/:id" component={EditTodo} />
