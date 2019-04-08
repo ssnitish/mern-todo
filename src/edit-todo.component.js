bedfc19c3d06622bc3f05a3d50b01092667136a2
GouravMukherjee <gouravmukherjee09@gmail.com>
2019-04-08 15:34:05 +0530

    added routes for different components

---
+++ src/edit-todo.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class EditTodo extends React.Component {
+   render() {
+      return(
+         <div>
+            <p>Welcome to edit todo component</p>
+         </div>
+      );
+   }
+}
+
+export default EditTodo;
---
+++ src/todos-list.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+ class TodoListComponent extends Component {
+   render() {
+      return (
+         <div>
+            Todos list component works!
+         </div>
+      );
+   }
+}
+
+export default TodoListComponent;
