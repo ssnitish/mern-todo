2a27f0faebbb6127cf99d3caa00702827baa90dd
GouravMukherjee <gouravmukherjee09@gmail.com>
2019-04-09 16:27:36 +0530

    Added  logic to edit todo component

--- src/edit-todo.component.js
+++ src/edit-todo.component.js
@@ -1,10 +1,119 @@
 import React from 'react';
+import axios from 'axios';
 
 class EditTodo extends React.Component {
+   baseUrl = "http://192.168.0.144:3006/api/todo"
+   state = {
+      description: "",
+      responsible: "",
+      priority: "",
+      completed: false,
+      status: "",
+      error: false
+   }
+   handleInput = (event, selector) => {
+      // console.log(event.target.value, selector);
+      const value = event.target.value;
+      switch (selector) {
+         case 'description': this.setState({ description: value }); break;
+         case 'responsible': this.setState({ responsible: value }); break;
+         case 'priority': this.setState({ priority: value }); break;
+      }
+   }
+   handleSubmit(event) {
+      event.preventDefault();
+      const { id } = this.props.location.state;
+      axios.patch(`${this.baseUrl}/todos/${id}`, this.state)
+         .then(response => {
+            console.log(`${JSON.stringify(response, null, 2)}\npudated successfully`);
+            this.setState({
+               status: "Todo updated successfully",
+               error: false
+            })
+         })
+         .catch(error => {
+            console.log(`error occured\n${JSON.stringify(error, null, 2)}`)
+            this.setState({
+               status: "Unable to update todo. Please try again",
+               error: true
+            })
+         })
+   }
    render() {
-      return(
-         <div>
-            <p>Welcome to edit todo component</p>
+      let status;
+      if (this.state.status) {
+         status = (
+            <div className={this.state.error ? "alert alert-danger" : "alert alert-success"} role="alert">
+               {this.state.status}
+            </div>
+         )
+      } else {
+         status = "";
+      }
+      return (
+         <div className="container">
+            {status}
+            <form onSubmit={(event) => this.handleSubmit(event)}>
+               <div className="form-group">
+                  <label htmlFor="exampleInputEmail1">Todo Description</label>
+                  <input
+                     type="text"
+                     className="form-control"
+                     id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Todo Description"
+                     required={true}
+                     onInput={(event) => this.handleInput(event, 'description')} />
+               </div>
+               <div className="form-group">
+                  <label htmlFor="exampleInputPassword1">Enter responsible</label>
+                  <input
+                     type="text"
+                     className="form-control"
+                     id="exampleInputPassword1"
+                     placeholder="Enter responsible"
+                     required={true}
+                     onInput={(event) => this.handleInput(event, 'responsible')} />
+               </div>
+               <div className="d-flex mb-1">Select priority</div>
+               <div className="d-flex mb-2">
+                  <div className="form-check mr-3">
+                     <input
+                        className="form-check-input"
+                        type="radio"
+                        name="exampleRadios"
+                        id="exampleRadios1"
+                        value="low"
+                        required={true}
+                        checked={this.state.priority === "low"}
+                        onChange={(event) => this.handleInput(event, 'priority')} />
+                     <label className="form-check-label" htmlFor="exampleRadios1">Low</label>
+                  </div>
+                  <div className="form-check mr-3">
+                     <input
+                        className="form-check-input"
+                        type="radio"
+                        name="exampleRadios"
+                        id="exampleRadios1"
+                        value="medium"
+                        required={true}
+                        checked={this.state.priority === "medium"}
+                        onChange={(event) => this.handleInput(event, 'priority')} />
+                     <label className="form-check-label" htmlFor="exampleRadios1">Medium</label>
+                  </div>
+                  <div className="form-check mr-3">
+                     <input
+                        className="form-check-input"
+                        type="radio"
+                        name="exampleRadios"
+                        id="exampleRadios1"
+                        value="high"
+                        required={true}
+                        checked={this.state.priority === "high"}
+                        onChange={(event) => this.handleInput(event, 'priority')} />
+                     <label className="form-check-label" htmlFor="exampleRadios1">High</label>
+                  </div>
+               </div>
+               <button type="submit" className="btn btn-primary">Edit Todo</button>
+            </form>
          </div>
       );
    }
--- src/navbar.component.js
+++ src/navbar.component.js
@@ -4,14 +4,17 @@ class NavBarComponent extends React.Component {
    render() {
       return (
          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to="/"> Todo </Link>
+            <Link to="/" className="navbar-brand"> Todo App</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                <ul className="navbar-nav mr-auto">
                   <li className="nav-item active">
-                    <Link to="/create" className="nav-link">Create </Link>
+                    <Link to="/" className="nav-link">View Todos</Link>
+                  </li>
+                  <li className="nav-item ">
+                    <Link to="/create" className="nav-link">Create Todo</Link>
                   </li>
                   {/* <li className="nav-item">
                      <Link to="/edit/123"><a className="nav-link">Edit</a></Link>
